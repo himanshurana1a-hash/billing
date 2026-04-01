78dc6aacfd71979bc7446a455645ad77f7cb59cb
shubhamkumar570 <shubhamkumardhiman04@gmailcom>
2026-04-01 11:26:37 +0530

    Add initial project setup with assets, styles, and configuration
    - Added various image assets including Frame.png, Rectangle.png, bgimage.png, hero.png, invoice.png, man.png, rocket.png, react.svg, and vite.svg.
    - Created index.css for Tailwind CSS integration.
    - Implemented main.jsx to set up the React application with StrictMode and render App and Home components.
    - Configured Vite with React and Tailwind CSS plugins in vite.config.js.

--- src/Home.jsx
+++ src/Home.jsx
@@ -706,8 +706,8 @@ function Home() {
                         <ul className='space-y-1 test-sm'>
 
                             <li>Contact Us</li>
-                            <li>help</li>
-                            <li>Training</li>
+                            <li>help & Support</li>
+                            
                         </ul>
                     </div>
 
@@ -715,6 +715,76 @@ function Home() {
                 </div>
 
             </footer>
+             <footer className='bg-gray-100 text-black-500 '>
+                <div className=' mx-auto px-6 grid md:grid-cols-4 py-6 gap-8'>
+
+                    <div>
+                        <h3 className='text-black text-xl  font-semibold mb-2'>Company</h3>
+                        <ul className='space-y-1 test-sm'>
+
+                            <li>About Us</li>
+                            <li>Contact Us</li>
+                            <li>Privacy Policy</li>
+                            <li>Terms & Conditions</li>
+                            <li>Pricing Policy</li>
+                            <li>Refundd Pilicy</li>
+                            <li>Cancellation Policy</li>
+                            <li>Cookie Policy</li>
+                            <li>Disclaimer</li>
+                            <li>Compliance Policy</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h3 className='text-black text-xl font-semibold mb-2'>Resources</h3>
+                        <ul className='space-y-1 test-sm'>
+
+                            <li>Templates & Downloads</li>
+                            <li>Free tools</li>
+                            <li>Videoa & Demos</li>
+                            <li>Help & Support</li>
+                            <li>security & Legal</li>
+                            <li>Updates & Knowlrdge</li>
+                            <li>Blog</li>
+                            <li>FAQs</li>
+
+                        </ul>
+                    </div>
+                    <div>
+                        <h3 className='text-black text-xl font-semibold mb-2'>Guides</h3>
+                        <ul className='space-y-1 test-sm'>
+
+                            <li>Invoicing Guides</li>
+                            <li>Payments & Checkout</li>
+                            <li>Pricing & Subscription</li>
+                            <li>Inventory Management</li>
+                            <li>GST & Compliance</li>
+                            <li>Report & Analytics </li>
+                            <li>E-invoice</li>
+                            <li>Case studt</li>
+                            
+                        </ul>
+                    </div>
+                    <div>
+                        <h3 className='text-blck text-xl font-semibold mb-2'>GST</h3>
+                        <ul className='space-y-1 test-sm'>
+
+                            <li>GST Basics</li>
+                            <li>GST Registration</li>
+                            <li>GST Returns</li>
+                            <li>GST Payment & Refunds</li>
+                            <li>GST Invoices & E-Invoicing</li>
+                            <li>Input Tax Credit (ITC)</li>
+                            <li>GST Penalties & Compliance</li>
+                        </ul>
+                    </div>
+
+
+                </div>
+
+            </footer>
+
+
+
 
 
 
